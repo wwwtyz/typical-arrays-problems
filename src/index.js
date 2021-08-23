2dc9225287fe0810c65ff3eec9950f2f5565793f
wwwtyz <wwwtyz@tut.by>
2021-08-23 16:03:13 +0300

    feature: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    let result = 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else
+    result = array.sort((a,b) => a - b)[0];
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else
+    result = array.sort((a,b) => b - a)[0];
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result = 0;
+    let sum = 0;
+    let i = 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+    array.forEach ((i) => {
+       sum += i;
+       result = sum / array.length;
+    })
+    };
+  return result;
 }
